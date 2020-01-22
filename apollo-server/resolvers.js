a86f029fe8b66f84fe17f5fe893b17bf1344db6c
rokiyama <57606749+rokiyama@users.noreply.github.com>
2020-01-22 18:59:15 +0900

    basic vue-apollo implementation

---
+++ apollo-server/resolvers.js
@@ -0,0 +1,24 @@
+import GraphQLJSON from "graphql-type-json";
+
+export default {
+  JSON: GraphQLJSON,
+
+  Query: {
+    hello: (root, { name }) =>
+      `Hello ${name || "World"}! ${new Date().toISOString()}`
+  },
+
+  Mutation: {
+    myMutation: (root, args, context) => {
+      const message = "My mutation completed!";
+      context.pubsub.publish("hey", { mySub: message });
+      return message;
+    }
+  },
+
+  Subscription: {
+    mySub: {
+      subscribe: (parent, args, { pubsub }) => pubsub.asyncIterator("hey")
+    }
+  }
+};
